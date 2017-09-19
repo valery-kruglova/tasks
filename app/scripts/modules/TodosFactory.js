4902ab67b5a19aa612d3470bb66a0e2493ccca87
Umer Hurramov <umer.hurramov@gmail.com>
2017-09-19 22:08:14 +0300

    [Add] module refactoring

---
+++ app/scripts/modules/TodosFactory.js
@@ -0,0 +1,37 @@
+const TodosFactory = {
+  createList: function (list) {
+    let items = [];
+    if (Array.isArray(list)) {
+      items = list.slice();
+    } else if (typeof list === 'string') {
+      items.push(list);
+    }
+    return {
+      add: function (item) {
+        if (Array.isArray(item)) {
+          for (let i = 0; i < item.length; i++) {
+            items.push(item[i]);
+          }
+        } else if (typeof item === 'string') {
+          items.push(item);
+        }
+      },
+      getList: function () {
+        return items.slice();
+      },
+      remove: function (removeItem) {
+        if (typeof removeItem === 'string') {
+          for (let i = items.length; i >= 0; i--) {
+            if (removeItem === items[i]) {
+              items.splice(i, 1);
+            }
+          }
+        } else if (typeof removeItem === 'number' && removeItem < items.length) {
+          items.splice(removeItem, 1);
+        }
+      }
+    }
+  }
+}
+
+export default TodosFactory
